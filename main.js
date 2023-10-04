369f54657d3bf10b7047f14186d316ddec366d58
tw-abiyanto <triwahyuabiyanto@outlook.com>
2023-10-04 08:04:11 +0000

    Check player win condition

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ function getComputerChoice() {
   return randomize(choice);
 }
 
+function checkWinCondition(player1, player2) {
+  if (player1 === player2) {
+    return "draw";
+  }
+
+  if (player1 === "rock" && player2 === "paper") {
+    return "lose";
+  } else if (player1 === "paper" && player2 === "scissor") {
+    return "lose";
+  } else if (player1 === "scissor" && player2 === "rock") {
+    return "lose";
+  } else {
+    return "win";
+  }
+}
+
 // TEST
 function testRandomize() {
   const arr = ["rock", "paper", "scissor"];
@@ -24,5 +40,46 @@ function testRandomize() {
   return 0;
 }
 
+function testPlayRound() {
+  const case1 = checkWinCondition("rock", "rock");
+  const case2 = checkWinCondition("rock", "paper");
+  const case3 = checkWinCondition("rock", "scissor");
+  const case4 = checkWinCondition("paper", "rock");
+  const case5 = checkWinCondition("paper", "paper");
+  const case6 = checkWinCondition("paper", "scissor");
+  const case7 = checkWinCondition("scissor", "rock");
+  const case8 = checkWinCondition("scissor", "paper");
+  const case9 = checkWinCondition("scissor", "scissor");
+
+  case1 === "draw"
+    ? console.log("Test pass")
+    : console.log(`Tess fail: rock vs rock should draw, instead ${case1}`);
+  case2 === "lose"
+    ? console.log("Test pass")
+    : console.log(`Test fail: rock vs paper should lose, instead ${case2}`);
+  case3 === "win"
+    ? console.log("Test pass")
+    : console.log(`Test fail: rock vs scissor should win, instead ${case3}`);
+  case4 === "win"
+    ? console.log("Test pass")
+    : console.log(`Test fail: paper vs rock should win, instead ${case4}`);
+  case5 === "draw"
+    ? console.log("Test pass")
+    : console.log(`Tess fail: paper vs paper should draw, instead ${case5}`);
+  case6 === "lose"
+    ? console.log("Test pass")
+    : console.log(`Test fail: paper vs scissor should lose, instead ${case6}`);
+  case7 === "lose"
+    ? console.log("Test pass")
+    : console.log(`Test fail: scissor vs rock should lose, instead ${case7}`);
+  case8 === "win"
+    ? console.log("Test pass")
+    : console.log(`Test fail: scissor vs paper should win, instead ${case8}`);
+  case9 === "draw"
+    ? console.log("Test pass")
+    : console.log(`Tess fail: rock vs rock should draw, instead ${case9}`);
+}
+
 // uncomment to run test
 // testRandomize();
+testPlayRound();
